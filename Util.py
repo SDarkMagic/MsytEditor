486c7ae3cb4c3b9d71cd2185f552f577c3f60564
dragonbuilderx <dragonbuilderx@gmail.com>
2021-02-05 17:35:31 +0000

    Base commit, sorry about random packager files...

---
+++ Util.py
@@ -0,0 +1,14 @@
+import pymsyt
+import pathlib
+
+def getMsbtAsDict(pathIn):
+  fileLoc = pathlib.Path(pathIn)
+  with open(fileLoc, 'rb') as readData:
+    msyt = pymsyt.Msbt.from_binary(readData)
+  return msyt.to_dict()
+
+class Msyt:
+  def __init__(self, filePathIn):
+    with open(pathlib.Path(filePathIn), 'rb') as readFile:
+      self.msbtDict = pymsyt.Msbt.from_binary(readFile.read())
+    print(self.mstbDict)
---
+++ main.py
@@ -0,0 +1,5 @@
+#import pymsyt
+#from . import Util as util
+import os
+import pathlib
+import pywebview
